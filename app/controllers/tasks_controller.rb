8a36ba2ad72a098268ad2bfcc59b555963551dab
Emmanuel Corrales <contact@emcorrales.com>
2026-01-09 20:08:07 +0800

    Generate TasksController.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,77 @@
+class TasksController < ApplicationController
+  include Authenticable
+
+  before_action :set_task, only: [:show, :update, :destroy, :reorder]
+
+  def index
+    @tasks = current_user.tasks.ordered
+    render json: @tasks, status: :ok
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    @task.position = current_user.tasks.maximum(:position).to_i + 1
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @task, status: :ok
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task, status: :ok
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    render json: { message: "Task deleted successfully" }, status: :ok
+  end
+
+  def reorder
+    new_position = reorder_params[:position].to_i
+    reorder_task(@task, new_position)
+    render json: @task, status: :ok
+  end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Task not found" }, status: :not_found
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
+
+  def reorder_params
+    params.require(:task).permit(:position)
+  end
+
+  def reorder_task(task, new_position)
+    old_position = task.position
+    user_tasks = current_user.tasks.ordered
+
+    if new_position < old_position
+      # Moving up: increment positions of tasks between new_position and old_position
+      user_tasks.where("position >= ? AND position < ?", new_position, old_position)
+        .update_all("position = position + 1")
+    elsif new_position > old_position
+      # Moving down: decrement positions of tasks between old_position and new_position
+      user_tasks.where("position > ? AND position <= ?", old_position, new_position)
+        .update_all("position = position - 1")
+    end
+
+    task.update(position: new_position)
+  end
+end
