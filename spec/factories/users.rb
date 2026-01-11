2a98e699d26470b191faaf7f02b5c3119998476b
Emmanuel Corrales <contact@emcorrales.com>
2026-01-12 05:02:04 +0800

    Make random user to avoid collisions during test.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
+require 'securerandom'
+
 FactoryBot.define do
   factory :user do
-    sequence(:username) { |n| "user#{n}" }
+    sequence(:username) { |n| "user-#{SecureRandom.hex(4)}-#{n}" }
     password { "password" }
   end
 end
